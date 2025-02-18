cf019429c11539f7df2dd1bae7a477c194ec5c0d
HirahataArthur <hirahataarthur@gmail.com>
2025-02-18 14:12:54 +0000

    xadrez aventureiro adicionado o movimento do cavalo em 'L' (2 casas para baixo e uma para a esquerda) utilizacao de loop aninhado no movimento do cavalo

--- xadrez.c
+++ xadrez.c
@@ -5,30 +5,18 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
     printf("Torre\n\n"); // movimento da torre
     for (int t = 1; t <= 5; t++)//utilizando o 'for' para imprimir o numero de casas andadas pela torre para a direita
     {
-        printf("Direita %d\n", t);
+        printf("Direita\n");
     }
-
 
     printf("\n\nBispo\n\n");// utilizando o 'while' para imprimir o numero de casas andadas pelo bisto para cima e direita
     int b=1; // variavel b = BISPO
     while (b <=5) // enquanto bispo for menor ou igual a 5, imprimir 'Cima direita' + numero da casa
     {
-       printf("Cima direita %d\n", b);
+       printf("Cima\n");
+       printf("Direita\n");
        b++;// adicionar 1 casa cada vez que impressa a informacao
     }
 
@@ -36,9 +24,28 @@ int main() {
     int r =1;// variavel r = RAINHA
     do
     {         // imprime 1 primeira vez sem considerar a condicao
-        printf("Esquerda %d\n", r);  // depois adiciona +1 casa e imprime novamente ate que 'r' seja maior que 8
+        printf("Esquerda\n");  // depois adiciona +1 casa e imprime novamente ate que 'r' seja maior que 8
         r++; // para impedir loop infinito e adicionar uma casa sempre que houver impressao
     } while (r <= 8); // condicao para repeticao do comando dentro de 'do'
+    
+    printf("\n\nCavalo\n\n");
+    int knightmove = 1; //variavel declarada para movimento do cavalo
+
+    //loop aninhado para o movimento do cavalo
+    do //loop externo
+    {
+
+        for (int i = 1; i <= 2; i++) // loop interno
+        {
+            printf("Baixo\n"); //imprime 2x, pois o loop so eh interrompido uma vez que 'i' seja maior que 2, o que requer 2 execucoes
+        }
+        printf("Esquerda\n"); /* imprime apenas 1x, pois o loop externo so age enquanto 'knightmove' for diferente de 0, 
+        uma vez que se subtrai 1 a cada loop, e a variavel vale inicialmente 1, entao eh necessario apenas 1 execucao completa do loop externo,
+        resultando em apenas 1 impressao*/
+
+    knightmove--; //subtrai 1 de 'knightmove' a cada execucao completa o loop
+    } while (knightmove !=0);
+    
     
     
     
